945cf20fbfad62dd4d2c0a3d4bb835d8bf7c1ea4
Talles Alves <talves2503@gmail.com>
2023-09-01 20:17:31 -0300

    add copy result function

--- index.js
+++ index.js
@@ -79,3 +79,15 @@ document.getElementById("switchTheme").addEventListener("click", function () {
         body.dataset.theme = "dark"
     }
 })
+
+document.getElementById("copy").addEventListener("click", function (ev) {
+    const button = ev.currentTarget;
+    if(button.innerText === "Copy"){
+        button.innerText = "Copied!"
+        button.classList.add("success")
+        navigator.clipboard.writeText(resultInput.value);
+    } else {
+        button.innerText = "Copy"
+        button.classList.remove("success")
+    }
+})
