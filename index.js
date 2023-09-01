a7cf82f032b1dab98049c5d61c5f05d6d1ea39d0
Talles Alves <talves2503@gmail.com>
2023-09-01 20:20:40 -0300

    add error treatment

--- index.js
+++ index.js
@@ -56,10 +56,13 @@ input.addEventListener("keydown", function (ev) {
 });
 
 function calculate() {
+    resultInput.value = "ERROR!"
+    resultInput.classList.add("error")
+    
     const result = eval(input.value)
     resultInput.value = result;
-    input.value = "";
-    input.focus();
+
+    resultInput.classList.remove("error")
 }
 
 document.getElementById("switchTheme").addEventListener("click", function () {
